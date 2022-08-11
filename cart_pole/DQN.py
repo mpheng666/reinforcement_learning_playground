87dc0cefbce4e3b2bbe937d216e8fa1b8d799ceb
mpheng666 <mpheng96@gmail.com>
2022-08-12 02:22:17 +0800

    add comment for understanding

--- cart_pole/DQN.py
+++ cart_pole/DQN.py
@@ -26,6 +26,7 @@ plt.ion()
 
 # if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print("device: ", device)
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
@@ -63,10 +64,16 @@ class DQN(nn.Module):
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
+        # print("w", w)
+        # print("h", h)
+
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        # print("convw", convw)
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        # print("convh", convh)
         linear_input_size = convw * convh * 32
         self.head = nn.Linear(linear_input_size, outputs)
+        # print("head:", self.head)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
@@ -75,6 +82,7 @@ class DQN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
+        # print("forward return: ", self.head(x.view(x.size(0), -1)))
         return self.head(x.view(x.size(0), -1))
 
 
@@ -92,12 +100,17 @@ def get_cart_location(screen_width):
 def get_screen():
     # Returned screen requested by gym is 400x600x3, but is sometimes larger
     # such as 800x1200x3. Transpose it into torch order (CHW).
+    # print("env.render.shape: ", env.render(mode='rgb_array').shape)
     screen = env.render(mode='rgb_array').transpose((2, 0, 1))
     # Cart is in the lower half, so strip off the top and bottom of the screen
     _, screen_height, screen_width = screen.shape
+    # print("screen.shape: ", screen.shape)
     screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
+    # print("screen2.shape: ", screen.shape)
     view_width = int(screen_width * 0.6)
+    # print("view_width: ", view_width)
     cart_location = get_cart_location(screen_width)
+    # print("cart location: ", cart_location)
     if cart_location < view_width // 2:
         slice_range = slice(view_width)
     elif cart_location > (screen_width - view_width // 2):
@@ -105,28 +118,30 @@ def get_screen():
     else:
         slice_range = slice(cart_location - view_width // 2,
                             cart_location + view_width // 2)
+    # print("slice_range: ", slice_range)
     # Strip off the edges, so that we have a square image centered on a cart
     screen = screen[:, :, slice_range]
     # Convert to float, rescale, convert to torch tensor
     # (this doesn't require a copy)
     screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    # print("screen continuous shape: ", screen.shape)
     screen = torch.from_numpy(screen)
     # Resize, and add a batch dimension (BCHW)
     return resize(screen).unsqueeze(0)
 
 
 env.reset()
-plt.figure()
-plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
-           interpolation='none')
-plt.title('Example extracted screen')
-plt.show()
+# plt.figure()
+# plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
+#            interpolation='none')
+# plt.title('Example extracted screen')
+# plt.show()
 
 BATCH_SIZE = 128
 GAMMA = 0.999
 EPS_START = 0.9
 EPS_END = 0.05
-EPS_DECAY = 100
+EPS_DECAY = 100 #200
 TARGET_UPDATE = 10
 
 # Get screen size so that we can initialize layers correctly based on shape
@@ -139,11 +154,14 @@ _, _, screen_height, screen_width = init_screen.shape
 n_actions = env.action_space.n
 
 policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+# print("policy net: ", policy_net)
 target_net = DQN(screen_height, screen_width, n_actions).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
+# print("target net: ", target_net)
 
 optimizer = optim.RMSprop(policy_net.parameters())
+# print("optimizer: ", optimizer)
 memory = ReplayMemory(10000)
 
 
@@ -153,8 +171,9 @@ steps_done = 0
 def select_action(state):
     global steps_done
     sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-        math.exp(-1. * steps_done / EPS_DECAY)
+    # print("sample: ", sample)
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
+    # print("eps_threshold: ", eps_threshold)
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
@@ -188,9 +207,10 @@ def plot_durations():
 
 
 def optimize_model():
-    if len(memory) < BATCH_SIZE:
+    if len(memory) < BATCH_SIZE: #128
         return
     transitions = memory.sample(BATCH_SIZE)
+    # print("transitions: ", transitions)
     # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
     # detailed explanation). This converts batch-array of Transitions
     # to Transition of batch-arrays.
@@ -200,9 +220,12 @@ def optimize_model():
     # (a final state would've been the one after which simulation ended)
     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
                                             batch.next_state)), device=device, dtype=torch.bool)
+    # print("non final mask: ", non_final_mask)
     non_final_next_states = torch.cat([s for s in batch.next_state
                                        if s is not None])
+    # print("non final next states: ", non_final_next_states.shape)
     state_batch = torch.cat(batch.state)
+    # print(type(state_batch))
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
 
@@ -210,6 +233,7 @@ def optimize_model():
     # columns of actions taken. These are the actions which would've been taken
     # for each batch state according to policy_net
     state_action_values = policy_net(state_batch).gather(1, action_batch)
+    # print("state_action_values size", state_action_values.size())
 
     # Compute V(s_{t+1}) for all next states.
     # Expected values of actions for non_final_next_states are computed based
@@ -217,25 +241,32 @@ def optimize_model():
     # This is merged based on the mask, such that we'll have either the expected
     # state value or 0 in case the state was final.
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    # print(type(next_state_values))
     next_state_values[non_final_mask] = target_net(
         non_final_next_states).max(1)[0].detach()
+
     # Compute the expected Q values
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+    # print("expected_state_action_values", expected_state_action_values.size())
 
     # Compute Huber loss
     criterion = nn.SmoothL1Loss()
     loss = criterion(state_action_values,
                      expected_state_action_values.unsqueeze(1))
+    print("loss", loss)
 
     # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
+    optimizer.zero_grad() # zero previos grad from backpropagation
+    loss.backward() # back propagation
     for param in policy_net.parameters():
+        # print("param.grad.data before clamp: ", param.grad.data)
         param.grad.data.clamp_(-1, 1)
-    optimizer.step()
+        # print("param.grad.data after clamp: ", param.grad.data)
+    optimizer.step() # gradient descent
 
 
-num_episodes = 1500
+num_episodes = 500
+num_episodes = 10
 for i_episode in range(num_episodes):
     # Initialize the environment and state
     env.reset()
